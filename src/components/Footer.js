0809cef1ea477c34d1c2530493ee438913100aad
Bharat Ranjan <bhaarat.ranjan@gmail.com>
2022-05-03 11:44:55 +0530

    first-commit

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer class="main-footer">
+        <strong>
+          Copyright &copy; 2022{" "}
+          <a href="https://github.com/codwithbharat">Bharat Ranjan</a>.
+        </strong>
+        All rights reserved.
+        <div class="float-right d-none d-sm-inline-block">
+          <b>Version</b> 3.1.0
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
